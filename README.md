85477b2c9ea40c938a1c99ffdd8e4ce9851c772b
ogawa <ogawa@tagbangers.co.jp>
2016-08-29 01:37:19 +0900

    Improve

---
+++ README.md
@@ -0,0 +1,15 @@
+Angular2 QuickStart By JSPM
+===========================
+
+BUILD
+-----
+```
+npm install
+```
+
+RUN
+---
+```
+npm start
+```
+
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,12 @@
 import gulp from 'gulp';
-import jspm from 'gulp-jspm';
-import useref from 'gulp-useref';
 import webserver from 'gulp-webserver';
-
-gulp.task('jspm', () => {
-	gulp.src(paths.app + 'main.js')
-		.pipe(jspm({
-			selfExecutingBundle: true
-		}))
-		.pipe(gulp.dest(paths.dest + 'static/app'));
-});
 
 gulp.task('webserver', () => {
 	gulp.src('src')
 		.pipe(webserver({
 			livereload: {
-				enable: true,
-				filter: (filename) => {
-					return !(filename.match(/.scss$/));
-				}
+				enable: true
 			},
 			open: true
 		}));
 });
-
-gulp.task('build', ['jspm']);
